8cacf945f146463d6858a1ca1cdf9a6eb06fab12
Blednykh Kirill <blednyhkirill@yandex.ru>
2025-10-18 20:37:14 +0300

    add json hadnler

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,12 +1,22 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/Guram-Gurych/shortenerURL.git/internal/service"
 	"github.com/go-chi/chi/v5"
 	"io"
 	"net/http"
+	"strings"
 )
+
+type RequestJSON struct {
+	URL string `json:"url"`
+}
+
+type ResponseJSON struct {
+	Result string `json:"result"`
+}
 
 type Handler struct {
 	service service.URLShortener
@@ -23,12 +33,12 @@ func NewHandler(s service.URLShortener, baseURL string) *Handler {
 func (h *Handler) Post(w http.ResponseWriter, r *http.Request) {
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		http.Error(w, "Не удалось прочитать тело запроса", http.StatusBadRequest)
+		http.Error(w, "Request body could not be read", http.StatusBadRequest)
 		return
 	}
 
 	if len(body) == 0 {
-		http.Error(w, "Тело запроса не может быть пустым", http.StatusBadRequest)
+		http.Error(w, "Request body cannot be empty.", http.StatusBadRequest)
 		return
 	}
 
@@ -36,7 +46,7 @@ func (h *Handler) Post(w http.ResponseWriter, r *http.Request) {
 
 	id, err := h.service.CreateShortURL(originalURL)
 	if err != nil {
-		http.Error(w, "Ошибка сервера", http.StatusInternalServerError)
+		http.Error(w, "Server error", http.StatusInternalServerError)
 		return
 	}
 
@@ -50,16 +60,52 @@ func (h *Handler) Post(w http.ResponseWriter, r *http.Request) {
 func (h *Handler) Get(w http.ResponseWriter, r *http.Request) {
 	id := chi.URLParam(r, "id")
 	if id == "" {
-		http.Error(w, "ID не может быть пустым", http.StatusBadRequest)
+		http.Error(w, "ID cannot be empty", http.StatusBadRequest)
 		return
 	}
 
 	originalURL, err := h.service.GetOriginalURL(id)
 	if err != nil {
-		http.Error(w, "URL не найден", http.StatusBadRequest)
+		http.Error(w, "URL not found", http.StatusBadRequest)
 		return
 	}
 
 	w.Header().Set("Location", originalURL)
 	w.WriteHeader(http.StatusTemporaryRedirect)
+}
+
+func (h *Handler) PostShorten(w http.ResponseWriter, r *http.Request) {
+	if !strings.Contains(r.Header.Get("Content-Type"), "application/json") {
+		http.Error(w, "invalid content type", http.StatusNotFound)
+		return
+	}
+
+	var req RequestJSON
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Failed to decode request body", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	if req.URL == "" {
+		http.Error(w, "URL field is missing", http.StatusBadRequest)
+		return
+	}
+
+	id, err := h.service.CreateShortURL(req.URL)
+	if err != nil {
+		http.Error(w, "Server error", http.StatusInternalServerError)
+		return
+	}
+
+	shortURL := fmt.Sprintf("%s/%s", h.baseURL, id)
+	resp := ResponseJSON{Result: shortURL}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	if err := json.NewEncoder(w).Encode(&resp); err != nil {
+		http.Error(w, "Failed to decode request body", http.StatusBadRequest)
+		return
+	}
 }
